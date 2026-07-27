29a308dfb15685190a33bbacdd7b1a44e5e7eb0c
Grace Xie <gracexie@berkeley.edu>
2026-07-27 06:18:05 -0700

    sweetshop page

--- events/sweet-shop/script.js
+++ events/sweet-shop/script.js
@@ -5,28 +5,34 @@ menuButton.addEventListener("click", () => {
     menu.classList.toggle("active");
 });
 
-const floatImg1 = document.getElementById("float-img1");
-const floatImg2 = document.getElementById("float-img2");
-const floatImg3 = document.getElementById("float-img3");
-
-const images1 = [
-    "../assets/images/homepage/about/Website Float 1-1.png",
-    "../assets/images/homepage/about/Website Float 1-2.png"
-];
-const images2 = [
-    "../assets/images/homepage/about/Website Float 2-1.png",
-    "../assets/images/homepage/about/Website Float 2-2.png"
-];
-const images3 = [
-    "../assets/images/homepage/about/Website Float 3-1.png",
-    "../assets/images/homepage/about/Website Float 3-2.png"
+const images = [
+    "../../assets/images/event/sweet-shop/examples/sweet1.png",
+    "../../assets/images/event/sweet-shop/examples/sweet2.png",
+    "../../assets/images/event/sweet-shop/examples/sweet3.png",
+    "../../assets/images/event/sweet-shop/examples/sweet4.jpg",
+    "../../assets/images/event/sweet-shop/examples/sweet5.png",
 ];
 
-let index = 0;
+let startIndex = 0;
 
-setInterval(() => {
-    index = (index + 1) % images1.length;
-    floatImg1.src = images1[index];
-    floatImg2.src = images2[index];
-    floatImg3.src = images3[index];
-}, 1200);
+const carouselImages = document.querySelectorAll(".carousel-img");
+
+function updateCarousel() {
+    carouselImages.forEach((img, i) => {
+        img.src = images[(startIndex + i) % images.length];
+    });
+}
+
+function changeSlide(direction) {
+    startIndex += direction;
+
+    if (startIndex < 0) {
+        startIndex = images.length - 1;
+    }
+
+    if (startIndex >= images.length) {
+        startIndex = 0;
+    }
+
+    updateCarousel();
+}
