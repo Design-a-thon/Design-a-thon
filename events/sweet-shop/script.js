1a468703ee4cc7d6652a3da2d88444dc192bee9c
Grace Xie <gracexie@berkeley.edu>
2026-07-27 20:09:47 -0700

    card descriptions

--- events/sweet-shop/script.js
+++ events/sweet-shop/script.js
@@ -50,3 +50,9 @@ window.addEventListener("resize", () => {
 });
 
 updateButtons();
+
+document.querySelectorAll(".carousel-card").forEach(card => {
+    card.addEventListener("click", () => {
+        card.classList.toggle("show-description");
+    });
+});
