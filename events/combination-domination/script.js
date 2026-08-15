72b4184ed7c427dd77ee5c03f3655a3b23d6be73
Grace Xie <gracexie@berkeley.edu>
2026-08-15 12:10:20 -0700

    implemented other pages for events

---
+++ events/combination-domination/script.js
@@ -0,0 +1,66 @@
+const menuButton = document.getElementById("menu-button");
+const menu = document.getElementById("menu");
+
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+
+const eventsButton = document.getElementById("current-link");
+const dropdown = document.querySelector(".dropdown-content");
+
+eventsButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    dropdown.classList.toggle("active");
+});
+
+const track = document.getElementById("carousel-track");
+const images = document.querySelectorAll(".carousel-img");
+
+function getVisible() {
+    return window.innerWidth <= 900 ? 1 : 3;
+}
+
+let index = 0;
+
+function getSlideWidth() {
+    return images[0].getBoundingClientRect().width + 25;
+}
+
+function moveCarousel() {
+    track.style.transition = "transform 0.5s ease";
+    track.style.transform = `translateX(-${index * getSlideWidth()}px)`;
+
+    updateButtons();
+}
+
+function updateButtons() {
+    document.getElementById("prev").disabled = index === 0;
+    document.getElementById("next").disabled =
+        index === images.length - getVisible();
+}
+
+document.getElementById("next").onclick = () => {
+    if (index < images.length - getVisible()) {
+        index++;
+        moveCarousel();
+    }
+};
+
+document.getElementById("prev").onclick = () => {
+    if (index > 0) {
+        index--;
+        moveCarousel();
+    }
+};
+
+window.addEventListener("resize", () => {
+    moveCarousel();
+});
+
+updateButtons();
+
+document.querySelectorAll(".carousel-card").forEach(card => {
+    card.addEventListener("click", () => {
+        card.classList.toggle("show-description");
+    });
+});
